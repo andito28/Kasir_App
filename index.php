7a10731bf98a821c4e24c1d748f169665bfe9dbb
andito28 <andito763@gmail.com>
2021-02-23 14:08:41 +0800

    create view

--- index.php
+++ index.php
@@ -8,10 +8,88 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
 
-    <title>Hello, world!</title>
+    <title>Kasir_App</title>
   </head>
   <body>
-    <h1>Hello, world!</h1>
+
+<div class="container">
+
+  <nav class="navbar bg-success">
+  <a class="navbar-brand text-white" href="#">
+    Kasir App
+  </a>
+  </nav>
+
+<div class="row pt-4">
+  <div class="col-md-4">
+  <div class="card">
+  <div class="card-header bg-success">
+    <ul class="nav nav-pills card-header-pills">
+      <li class="nav-item">
+        <a class="nav-link disabled text-white" href="#" tabindex="-1" aria-disabled="true">Transaction</a>
+      </li>
+    </ul>
+  </div>
+  <div class="card-body">
+  <form>
+  <div class="form-group">
+    <label for="exampleFormControlSelect1">Kode Barang</label>
+    <select class="form-control" id="exampleFormControlSelect1">
+      <option value="">1</option>
+    </select>
+  </div>
+
+  <button type="submit" class="btn btn-success btn-sm">Tambahkan</button>
+</form>
+  </div>
+</div>
+  </div>
+
+  <div class="col-md-8">
+
+  <div class="card">
+  <div class="card-header">
+    <ul class="nav nav-pills card-header-pills">
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true"><h6>TOTAL HARGA :</h6></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true"><h6>RP  30.000</h6></a>
+      </li>
+    </ul>
+  </div>
+  <div class="card-body">
+  <table class="table table-bordered">
+          <thead>
+            <tr>
+              <th scope="col">Barang</th>
+              <th scope="col">Harga</th>
+              <th scope="col">Qty</th>
+              <th scope="col">Aksi</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Mark</td> 
+              <td>Otto</td>
+              <td><input type="text" name="qty" value="1" style="width:50px"></td>
+              <td>X</td>
+            </tr>
+          </tbody>
+        </table>
+  </div>
+</div>
+
+  
+  </div>
+
+</div>
+
+<div class="footer pt-3">
+  <footer class="bg-success text-center p-3"></footer>
+</div>
+
+</div>
 
     <!-- Optional JavaScript; choose one of the two! -->
 
